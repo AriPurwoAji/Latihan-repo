a4b0cf90b9d2df863a91552a5d2ae063db995043
AriPurwoAji <ajiaripurwo@gmail.com>
2025-10-24 21:01:03 +0700

    senod commit

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,30 @@ class Myhome extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Demo Flutter App'),
+        backgroundColor: const Color.fromARGB(255, 105, 194, 239),
+        actions: [
+          Icon(Icons.person, color: Colors.blue),
+          SizedBox(width: 20),
+          Icon(Icons.account_tree),
+          SizedBox(width: 20),
+          Icon(Icons.ac_unit),
+          SizedBox(width: 20),
+          Icon(Icons.home),
+          SizedBox(width: 20),
+        ],
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text('Selamat datang di aplikasi demo flutter'),
+            Icon(Icons.flutter_dash, size: 50),
+          ],
+        ),
+      ),
+    );
   }
 }
